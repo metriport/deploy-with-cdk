bf38bd33ec19e4dedd5e4ec4cb61b3ce6457af21
Rafael Leite <rafael.leite@livechroma.ca>
2023-01-03 18:18:06 -0300

    Clean up action.yml and deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,41 +2,34 @@
 
 set -o errexit
 
-# Validate AWS Credentials
-
+# Validate required env vars
 if [ "${AWS_ACCESS_KEY_ID}" == "" ]; then
   echo "Input AWS_ACCESS_KEY_ID cannot be empty"
   exit 1
 fi
-
 if [ "${AWS_SECRET_ACCESS_KEY}" == "" ]; then
   echo "Input AWS_SECRET_ACCESS_KEY cannot be empty"
   exit 1
 fi
-
 if [ "${AWS_DEFAULT_REGION}" == "" ]; then
   echo "Input AWS_DEFAULT_REGION cannot be empty"
   exit 1
 fi
-
 if [ "${INPUT_CDK_ACTION}" == "" ]; then
   echo "Input INPUT_CDK_ACTION cannot be empty"
   exit 1
 fi
-
 if [ "${INPUT_CDK_ENV}" == "" ]; then
   echo "Input INPUT_CDK_ENV cannot be empty"
   exit 1
 fi
-
-pwd
 
 # echo "Installing AWS CLI ..."
 # curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 # unzip awscliv2.zip
 # ./aws/install
 
-# Configure AWS cli with a default profile
+# Configure AWS cli
 aws configure set aws_access_key_id "${AWS_ACCESS_KEY_ID}"
 aws configure set aws_secret_access_key "${AWS_SECRET_ACCESS_KEY}"
 aws configure set default.region "${AWS_DEFAULT_REGION}"
@@ -44,19 +37,22 @@ aws configure set default.region "${AWS_DEFAULT_REGION}"
 ## Install AWS CDK
 [[ -z "${INPUT_CDK_VERSION}" ]] && npm install -g aws-cdk || npm install -g aws-cdk@"${INPUT_CDK_VERSION}"
 
+echo "Installing INFRA dependencies..."
+pwd
 cd "${GITHUB_WORKSPACE}/infra"
 pwd
-echo "Installing INFRA dependencies..."
 npm install
-echo "====== DONE ======"
+echo "...DONE INFRA dependencies"
+
 echo "CDK version: $(cdk --version)"
 echo "Region: ${AWS_DEFAULT_REGION}"
 echo "Env: ${INPUT_CDK_ENV}"
 echo "Stack: ${INPUT_CDK_STACK}"
 echo "Action: ${INPUT_CDK_ACTION}"
 
-echo "Bootstraping..."
+echo "CDK Bootstraping..."
 cdk bootstrap -c env=${INPUT_CDK_ENV}
+echo "...DONE CDK Bootstraping..."
 
 # Run cdk for a specific stack
 if [[ "${INPUT_CDK_STACK}" != '' ]]; then
